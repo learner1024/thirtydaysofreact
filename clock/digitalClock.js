797581b2efa9759a17237e09b6d9fe220593eb7d
Sandeep Khandewale <learner1024@gmail.com>
2017-03-01 17:55:28 +0530

    unfinished digital clock

--- clock/digitalClock.js
+++ clock/digitalClock.js
@@ -1,7 +1,31 @@
 class DigitalClock extends React.Component{
+    constructor(props){
+        super(props);
+
+        //initial state
+        
+        this.state = this.getStateObj();
+
+        setInterval(this.updateState.bind(this), 1000);
+    }
+
+    updateState(){
+        this.setState(this.getStateObj());
+    }
+
+    getStateObj(){
+        const now = new Date();
+        return {
+            hours: now.getHours(),
+            minutes: now.getMinutes(),
+            seconds: now.getSeconds()
+        }
+    }
+
     render(){
+        const {hours, minutes, seconds} = this.state;
         return (
-            <div className="digital-clock">HH : MM : SS : AM/PM</div>
+            <div className="digital-clock">{hours} : {minutes} : {seconds}</div>
         );
     }
 }
