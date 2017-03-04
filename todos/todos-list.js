85ccbe76b5a5677cba75e77decbe382f0f9e8626
Sandeep Khandewale <learner1024@gmail.com>
2017-03-04 18:51:07 +0530

    react creates commentnodes i dont like that

--- todos/todos-list.js
+++ todos/todos-list.js
@@ -2,11 +2,10 @@ var TodosList = React.createClass({
     displayName: "TodosList",
     render: function(){
         return (
-            <ul>
-                {this.props.todos.map(function(t){
-                    return <li> {t} </li>
+            <ul>{this.props.todos.map(function(t){
+                    return <li>{t}</li>
                 })}
-            </ul>
+                </ul>
         )
     }
 })
